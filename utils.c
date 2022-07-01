fbd82136353e3a8b6dec936f29f2b11246158fcd
Tomasz Nal <tomasz.nal@sfi.pl>
2022-07-01 20:24:54 +0200

    move utils functions to utils.c

--- utils.c
+++ utils.c
@@ -1,22 +1,118 @@
 #include        <sys/types.h>   /* basic system data types */
 #include        <sys/socket.h>  /* basic socket definitions */
 #include        <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
-// #include        <arpa/inet.h>   /* inet(3) functions */
-// #include        <errno.h>
-// #include        <signal.h>
-// #include        <stdio.h>
 #include        <stdlib.h>
-// #include        <string.h>
-// #include 	<sys/ioctl.h>
-// #include 	<unistd.h>
 #include 	<net/if.h>
-// #include	<netdb.h>
-// #include	<sys/utsname.h>
-// #include	<linux/un.h>
-
 
 #define MAXLINE 1024
 #define SA      struct sockaddr
+
+
+
+///////////////////////
+///      Utils      ///
+///////////////////////
+void Fputs(const char *ptr, FILE *stream)
+{
+	if (fputs(ptr, stream) == EOF)
+		perror("fputs error");
+}
+
+char * Fgets(char *ptr, int n, FILE *stream)
+{
+	char	*rptr;
+
+	if ( (rptr = fgets(ptr, n, stream)) == NULL && ferror(stream))
+		perror("fgets error");
+
+	return (rptr);
+}
+/* Write "n" bytes to a descriptor. */
+ssize_t writen(int fd, const void *vptr, size_t n)
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (nwritten < 0 && errno == EINTR)
+				nwritten = 0;		/* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+/* end writen */
+
+void Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+		perror("writen error");
+}
+
+static int	read_cnt;
+static char	*read_ptr;
+static char	read_buf[MAXLINE];
+
+static ssize_t my_read(int fd, char *ptr)
+{
+
+	if (read_cnt <= 0) {
+again:
+		if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+			if (errno == EINTR)
+				goto again;
+			return(-1);
+		} else if (read_cnt == 0)
+			return(0);
+		read_ptr = read_buf;
+	}
+
+	read_cnt--;
+	*ptr = *read_ptr++;
+	return(1);
+}
+
+ssize_t readline(int fd, void *vptr, size_t maxlen)
+{
+	ssize_t	n, rc;
+	char	c, *ptr;
+
+	ptr = vptr;
+	for (n = 1; n < maxlen; n++) {
+		if ( (rc = my_read(fd, &c)) == 1) {
+			*ptr++ = c;
+			if (c == '\n')
+				break;	/* newline is stored, like fgets() */
+		} else if (rc == 0) {
+			*ptr = 0;
+			return(n - 1);	/* EOF, n - 1 bytes were read */
+		} else
+			return(-1);		/* error, errno set by read() */
+	}
+
+	*ptr = 0;	/* null terminate like fgets() */
+	return(n);
+}
+
+/* end readline */
+
+ssize_t Readline(int fd, void *ptr, size_t maxlen)
+{
+	ssize_t		n;
+
+	if ( (n = readline(fd, ptr, maxlen)) < 0)
+		perror("readline error");
+	return(n);
+}
+
 
 unsigned int
 _if_nametoindex(const char *ifname)
@@ -118,3 +214,52 @@ int mcast_set_loop(int sockfd, int onoff)
 		return(-1);
 	}
 }
+
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+	else
+		printf("Protocol not supported\n");
+		exit(1);
+}
+
+// NOT IMPLEMENTED:
+
+///////////////////////
+///     Argpars     ///
+///////////////////////
+#include <argp.h>
+#include <stdbool.h>
+
+const char *argp_program_version = "programname programversion";
+const char *argp_program_bug_address = "<your@email.address>";
+static char doc[] = "Your program description.";
+static char args_doc[] = "[FILENAME]...";
+static struct argp_option options[] = { 
+    { "line", 'l', 0, 0, "Compare lines instead of characters."},
+    { "word", 'w', 0, 0, "Compare words instead of characters."},
+    { "nocase", 'i', 0, 0, "Compare case insensitive instead of case sensitive."},
+    { 0 } 
+};
+
+struct arguments {
+    enum { CHARACTER_MODE, WORD_MODE, LINE_MODE } mode;
+    bool isCaseInsensitive;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = state->input;
+    switch (key) {
+    case 'l': arguments->mode = LINE_MODE; break;
+    case 'w': arguments->mode = WORD_MODE; break;
+    case 'i': arguments->isCaseInsensitive = true; break;
+    case ARGP_KEY_ARG: return 0;
+    default: return ARGP_ERR_UNKNOWN;
+    }   
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
